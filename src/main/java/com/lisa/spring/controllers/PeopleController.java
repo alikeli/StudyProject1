40724e8c88c5dc2c80894ef9d3c420d3c935e1a5
alikeli <eli-com2@yandex.ru>
2023-02-13 16:23:31 +0100

    Validations for forms

--- src/main/java/com/lisa/spring/controllers/PeopleController.java
+++ src/main/java/com/lisa/spring/controllers/PeopleController.java
@@ -2,9 +2,13 @@ package com.lisa.spring.controllers;
 
 import com.lisa.spring.dao.PersonDao;
 import com.lisa.spring.model.Person;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -35,7 +39,10 @@ public class PeopleController {
     }
 
     @PostMapping
-    public String create(@ModelAttribute("person") Person person) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "people/new";
         personDao.save(person);
         return "redirect:/people";
     }
@@ -48,7 +55,11 @@ public class PeopleController {
     }
 
     @PatchMapping("{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if(bindingResult.hasErrors())
+            return "people/edit";
         personDao.update(id, person);
         return "redirect:/people";
     }
