06555f92086188b63c6afb3712ae19f6e36e7997
alikeli <eli-com2@yandex.ru>
2023-02-14 21:45:09 +0100

    JDBC template, configurations from properties file. And batch update

--- src/main/java/com/lisa/spring/config/SpringConfig.java
+++ src/main/java/com/lisa/spring/config/SpringConfig.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
+
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -23,7 +25,7 @@ import java.util.Objects;
 @Configuration
 @ComponentScan("com.lisa.spring")
 @EnableWebMvc
-@PropertySource("classpath:dataBase.properties")
+@PropertySource("classpath:database.properties")
 public class SpringConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
--- src/main/java/com/lisa/spring/dao/PersonDao.java
+++ src/main/java/com/lisa/spring/dao/PersonDao.java
@@ -1,10 +1,12 @@
 package com.lisa.spring.dao;
 
 import com.lisa.spring.model.Person;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.spec.PSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,14 +39,113 @@ public class PersonDao {
 
     public void update(int id, Person updatePerson) {
         jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?",
-                updatePerson.getName(),updatePerson.getAge(), updatePerson.getEmail(), id);
+                updatePerson.getName(), updatePerson.getAge(), updatePerson.getEmail(), id);
     }
 
 
     public void delete(int id) {
-       jdbcTemplate.update("DELETE FROM Person WHERE id =?", id);
+        jdbcTemplate.update("DELETE FROM Person WHERE id =?", id);
 
     }
 
+    //Performance testing batch insert
+
+//    public void testMultipleUpdate() {
+//        List<Person> people = create1000People();
+//
+//        long before = System.currentTimeMillis();
+//
+//        for (Person person : people) {
+//            jdbcTemplate.update("INSERT INTO Person VALUES(?,?,?,?)", person.getId(), person.getName(), person.getAge(),
+//                    person.getEmail());
+//        }
+//
+//
+//        long after = System.currentTimeMillis();
+//        System.out.println("Time : " + (after - before));
+//    }
+//
+//    public void testButchUpdate() {
+//        List<Person> people = create1000People();
+//
+//        long before = System.currentTimeMillis();
+//
+//        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES (?,?,?,?)",
+//                new BatchPreparedStatementSetter() {
+//                    @Override
+//                    public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+//                        preparedStatement.setInt(1, people.get(i).getId());
+//                        preparedStatement.setString(2, people.get(i).getName());
+//                        preparedStatement.setInt(3, people.get(i).getAge());
+//                        preparedStatement.setString(4, people.get(i).getEmail());
+//                    }
+//
+//                    @Override
+//                    public int getBatchSize() {
+//                        return people.size();
+//                    }
+//                });
+//        long after = System.currentTimeMillis();
+//        System.out.println("Time : " + (after - before));
+//    }
+//
+//
+//    private List<Person> create1000People() {
+//        List<Person> people = new ArrayList<>();
+//        for (int i = 0; i < 1000; i++) {
+//            people.add(new Person(i, "name" + i, 30, "test" + i + "@mail.com"));
+//
+//        }
+//        return people;
+//    }
+
+
+    public void testMultipleUpdate() {
+        List<Person> people = create1000People();
+
+        long before = System.currentTimeMillis();
+
+        for (Person person : people) {
+            jdbcTemplate.update("INSERT INTO Person VALUES(?, ?, ?, ?)", person.getId(), person.getName(), person.getAge(),
+                    person.getEmail());
+        }
+
+        long after = System.currentTimeMillis();
+        System.out.println("Time: " + (after - before));
+    }
+
+    public void testButchUpdate() {
+        List<Person> people = create1000People();
+
+        long before = System.currentTimeMillis();
+
+        jdbcTemplate.batchUpdate("INSERT INTO Person VALUES(?, ?, ?, ?)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setInt(1, people.get(i).getId());
+                        ps.setString(2, people.get(i).getName());
+                        ps.setInt(3, people.get(i).getAge());
+                        ps.setString(4, people.get(i).getEmail());
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return people.size();
+                    }
+                });
+
+        long after = System.currentTimeMillis();
+        System.out.println("Time: " + (after - before));
+    }
+
+    private List<Person> create1000People() {
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++)
+            people.add(new Person(i, "Name" + i, 30, "test" + i + "@mail.ru"));
+
+        return people;
+    }
 
 }
