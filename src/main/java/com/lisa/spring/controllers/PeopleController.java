ee729dc76f46c899b56fba8296f50dcd008958f7
alikeli <eli-com2@yandex.ru>
2023-02-15 21:57:12 +0100

    validator

--- src/main/java/com/lisa/spring/controllers/PeopleController.java
+++ src/main/java/com/lisa/spring/controllers/PeopleController.java
@@ -3,6 +3,7 @@ package com.lisa.spring.controllers;
 import com.lisa.spring.dao.PersonDao;
 import com.lisa.spring.model.Person;
 
+import com.lisa.spring.util.PersonValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,9 +16,12 @@ import javax.validation.Valid;
 public class PeopleController {
 
     private final PersonDao personDao;
+    private final PersonValidator personValidator;
 
-    public PeopleController(PersonDao personDao) {
+
+    public PeopleController(PersonDao personDao, PersonValidator personValidator) {
         this.personDao = personDao;
+        this.personValidator = personValidator;
     }
 
 
@@ -41,6 +45,8 @@ public class PeopleController {
     @PostMapping
     public String create(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult) {
+
+        personValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors())
             return "people/new";
         personDao.save(person);
@@ -58,6 +64,7 @@ public class PeopleController {
     public String update(@ModelAttribute("person") @Valid Person person,
                          BindingResult bindingResult,
                          @PathVariable("id") int id) {
+        personValidator.validate(person, bindingResult);
         if (bindingResult.hasErrors())
             return "people/edit";
         personDao.update(id, person);
