760083d7e285bc5ee9f1a35af9d5469e0d893972
alikeli <eli-com2@yandex.ru>
2023-02-15 21:54:40 +0100

    validator

--- src/main/java/com/lisa/spring/dao/PersonDao.java
+++ src/main/java/com/lisa/spring/dao/PersonDao.java
@@ -6,10 +6,11 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.spec.PSource;
-import java.sql.*;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class PersonDao {
@@ -25,11 +26,18 @@ public class PersonDao {
         return jdbcTemplate.query("SELECT * FROM person", new BeanPropertyRowMapper<>(Person.class));
     }
 
+    public Optional<Person> show(String email) {
+        return jdbcTemplate.query("SELECT * FROM Person WHERE email = ?", new Object[] {email},
+                new BeanPropertyRowMapper<>(Person.class)).stream().findAny();
 
+    }
     public Person show(int id) {
-        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Person.class))
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id},
+                        new BeanPropertyRowMapper<>(Person.class))
                 .stream().findAny().orElse(null);
     }
+
+
 
     public void save(Person person) {
         jdbcTemplate.update("INSERT INTO Person(name,age,email) VALUES(?,?,?)", person.getName(), person.getAge(),
