95fe78e06b062ad6d965532a5bd6fbecd64acfb7
alikeli <eli-com2@yandex.ru>
2023-02-23 18:02:01 +0100

    date and time in hibernate

--- src/main/java/com/lisa/spring/model/Person.java
+++ src/main/java/com/lisa/spring/model/Person.java
@@ -1,12 +1,15 @@
 package com.lisa.spring.model;
 
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.persistence.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.List;
 
 
@@ -34,6 +37,14 @@ public class  Person {
     private String email;
     @OneToMany(mappedBy = "owner")
     private List<Item> items;
+    @Column(name = "date_of_birth")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date dateOfBirth;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
 
     public Person() {
 
@@ -75,6 +86,22 @@ public class  Person {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
     }
 
     @Override
